cd9af8fae7357acd5683ed228cca8b328fbb2150
Carlos de Jesus Gutierrez Loaeza <7153253-GutierrezLoaezaCarlosdeJesus@users.noreply.gitlab.com>
2021-01-02 15:43:14 -0600

    Reestructurado

--- js/main.js
+++ js/main.js
@@ -6,43 +6,8 @@ function Producto(nombre, codigo, existencia, precio) {
     this.precio = precio;
 }
 
-var p1 = new Producto("Coca-cola", 12200225, 15, 15.0);
-
-let productos = [p1];
-
-var p2 = new Producto("Barritas", 1250058, 20, 12.5)
-productos.push(p2);
-
-function agrega() {
-    var nombre = document.getElementById("nombre").value;
-    var codigo = document.getElementById("codigo").value;
-    var existencia = document.getElementById("existencia").value;
-    var precio = document.getElementById("precio").value;
-
-    var p = new Producto(nombre, codigo, existencia, precio)
-    productos.push(p);
-    console.log(productos.length);
-
-    document.getElementById("nombre").value = "";
-    document.getElementById("codigo").value = "";
-    document.getElementById("existencia").value = "";
-    document.getElementById("precio").value = "";
-
+function borraElemento(e){
+    var i = e.parentNode.parentNode.rowIndex;
+    document.getElementById("tabla").deleteRow(i);
 }
 
-function llenaTabla() {
-    var cuerpoTabla = document.getElementById("productos");
-    var tablita = "";
-    for (var i = 0; i < productos.length; i++) {
-        tablita += "<tr>";
-        tablita += ""+ (i+1);
-        tablita += "<td>" + productos[i].name + "<td>";
-        tablita += "<td>" + productos[i].codigo + "<td>";
-        tablita += "<td>" + productos[i].existencia + "<td>";
-        tablita += "<td>" + productos[i].precio + "<td>";
-        tablita += "<td><td>";
-        tablita += "<td><td>";
-        tablita += "<tr>";
-    }
-    document.getElementById("productos").innerHTML = tablita;
-}
