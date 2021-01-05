5ba261852e77249a087c2597e5e5ddb9a5f5ea86
Carlos de Jesus Gutierrez Loaeza <7153253-GutierrezLoaezaCarlosdeJesus@users.noreply.gitlab.com>
2021-01-05 12:13:30 -0600

    casi terminado

--- js/main.js
+++ js/main.js
@@ -6,8 +6,38 @@ function Producto(nombre, codigo, existencia, precio) {
     this.precio = precio;
 }
 
-function borraElemento(e){
+function borraElemento(e) {
     var i = e.parentNode.parentNode.rowIndex;
     document.getElementById("tabla").deleteRow(i);
 }
 
+var codigo_barras = new RegExp("\[0-9]{13}");
+var exist_reg = new RegExp("[0-9]+");
+var precio_reg = new RegExp("\[0-9]+[\.]?[0-9]+");
+
+function test(nombre, codigo, existencia, precio) {
+    var cod = codigo_barras.exec(codigo);
+    var exist = exist_reg.exec(existencia);
+    var pre = precio_reg.exec(precio);
+    if (!cod && !exist && !pre) {
+        alert("Código de barras invalido o existencia incorrecta o precio incorrecto ");
+    } else {
+        var p = new Producto(nombre, codigo, existencia, precio);
+        alert("Datos del producto correctos\nNombre:"+nombre+
+                "\nCódigo: "+codigo+"\nExistencia: "+existencia+"\nPrecio: "+precio);
+    }
+}
+
+function agrega() {
+    var nombre = document.getElementById("nombre").value;
+    var codigo = document.getElementById("codigo").value;
+    var existencia = document.getElementById("existencia").value;
+    var precio = document.getElementById("precio").value;
+
+    test(nombre, codigo, existencia, precio);
+
+    document.getElementById("nombre").value = "";
+    document.getElementById("codigo").value = "";
+    document.getElementById("existencia").value = "";
+    document.getElementById("precio").value = "";
+}
